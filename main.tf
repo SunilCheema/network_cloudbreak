2c247c399d368fa4ef9aa9b8b88f50066d4b27e1
SunilCheema <sunil03cheema@hotmail.co.uk>
2019-08-05 02:38:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,41 +5,6 @@ region     = "us-west-2"
 
 }
 
-locals {
-  instance-userdata = <<EOF
-  #!/bin/bash
-
-  yum -y update
-  yum -y install net-tools ntp wget lsof unzip tar iptables-services
-  systemctl enable ntpd && systemctl start ntpd
-  systemctl disable firewalld && systemctl stop firewalld
-  iptables --flush INPUT && \
-  iptables --flush FORWARD && \
-  service iptables save
-  setenforce 0
-  sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-  getenforce
-  yum install -y docker
-  systemctl start docker
-  systemctl enable docker
-  sed -i 's/journald/json-file/' /etc/sysconfig/docker
-  systemctl restart docker
-  systemctl status docker
-  yum -y install unzip tar
-  curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_2.8.0_$(uname)_x86_64.tgz | sudo tar -xz -C /bin cbd
-  cbd --version
-  mkdir cloudbreak-deployment
-  cd cloudbreak-deployment
-  curl https://ipinfo.io/ip > address.txt
-  ip=$(cat address.txt)
-  printf "export UAA_DEFAULT_SECRET=<secret>\nexport UAA_DEFAULT_USER_PW=<password>\nexport UAA_DEFAULT_USER_EMAIL=<email>\nexport PUBLIC_IP=$ip\n" > Profile
-  rm *.yml
-  cbd generate
-  cbd pull-parallel
-  sudo cbd start
-EOF
-}
-
 variable "instance_type_size" {
   type    = list(string)
   default = ["t2.medium"]
@@ -143,7 +108,7 @@ resource "aws_security_group" "allow_ssh_https" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["${var.ip_block}"]
+    cidr_blocks = ["${ip_block}"]
   }
 
   ingress {
@@ -153,7 +118,7 @@ resource "aws_security_group" "allow_ssh_https" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["${var.ip_block}"]
+    cidr_blocks = ["${ip_block}"]
   }
 
   egress {
@@ -176,6 +141,6 @@ subnet_id = "${aws_subnet.public_subnet.id}"
 vpc_security_group_ids = ["${aws_security_group.allow_ssh_https.id}"]
 associate_public_ip_address = true
 key_name = "OregonPair"
-user_data = "${local.instance-userdata}"
+user_data = "${file("installCloudbreak.sh")}"
 
 }
